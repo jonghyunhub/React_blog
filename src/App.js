0fdd9f0cdbb1429c16b5a5ad9023b3f76a36c3d2
chujonghyun <chuwhdgus@gmail.com>
2021-03-30 21:42:24 +0900

    1일차

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
+  let [title,titleFunc] = useState(['하이!','운동 좀 하자','잠이 안와..']);
+  let [numGood,numGoodFunc] = useState(0);
+  
+  let [modal, modalFunc] = useState(false);
+  
+  let posts = '배고파';
+
+  function titleChange(){
+    var newData = [...title];
+    newData.sort();
+    titleFunc( newData );
+  }
+
+  function ModalSee(){
+    modalFunc(!modal);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+        <button onClick={ titleChange }>새로고침</button>
+      </div>
+      <div className="list">
+        <h4>{ title[0] } 
+        <span onClick={function(){ numGoodFunc(numGood+1)}}>👍</span> 
+        {numGood} </h4>
+        <p>3월 30일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>3월 30일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>3월 30일 발행</p>
+        <hr/>
+      </div>
+      <button onClick={ ModalSee }>모달</button>
+
+      {
+        modal === true
+        ? <Modal/>
+        :null
+      }
     </div>
   );
+} 
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
+
+
